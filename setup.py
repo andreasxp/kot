d389968ae82499df7cbdffb6577fa3cd01c86378
andreasxp <28830446+andreasxp@users.noreply.github.com>
2022-04-24 15:43:46 +0300

    Make setup.py copy license file into dists

--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
 """Install script."""
 from setuptools import setup, find_packages
+from setuptools.command.egg_info import egg_info
+
+
+# See https://stackoverflow.com/a/66443941/9118363 for explanation
+class egg_info_ex(egg_info):
+    """Includes license file into `.egg-info` folder."""
+
+    def run(self):
+        # don't duplicate license into `.egg-info` when building a distribution
+        if not self.distribution.have_run.get('install', True):
+            # `install` command is in progress, copy license
+            self.mkpath(self.egg_info)
+            self.copy_file('LICENSE.txt', self.egg_info)
+
+        egg_info.run(self)
 
 
 install_requires = [
@@ -32,5 +47,7 @@ setup(
     package_data={
         "kot": ["data/*"]
     },
-    entry_points=entry_points
+    entry_points=entry_points,
+    license_files=["LICENSE.txt"],
+    cmdclass={"egg_info": egg_info_ex},
 )
